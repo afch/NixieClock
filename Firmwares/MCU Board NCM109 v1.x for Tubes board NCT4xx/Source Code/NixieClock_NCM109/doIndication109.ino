525f03008db30b7946b974c3fa8ce2b92984fb02
Aleksej Fomin <fominalec@gmail.com>
2017-12-08 19:59:58 +0200

    Firmware for NCM109

---
+++ Firmwares/MCU Board NCM109 v1.x for Tubes board NCT4xx/Source Code/NixieClock_NCM109/doIndication109.ino
@@ -0,0 +1,123 @@
+//driver for NCM109+NCT4XX series (register HV5122)
+//driver version 1.0
+//1 on register's output will turn on a digit 
+
+#include "doIndication109.h"
+
+#define UpperDotsMask 0x1000000
+#define LowerDotsMask 0x2000000
+
+void doIndication()
+{
+  static byte AnodesGroup=1;
+  unsigned long AnodesGroupMask;
+  static unsigned long lastTimeInterval1Started;
+  if ((micros()-lastTimeInterval1Started)>5000)
+  {
+   lastTimeInterval1Started=micros();
+   unsigned long var32=0;
+   unsigned long tmpVar; 
+
+  switch (AnodesGroup)
+   {
+   case 1:{AnodesGroupMask=AnodeGroup1ON; break;};
+   case 2:{AnodesGroupMask=AnodeGroup2ON; break;};
+   case 3:{AnodesGroupMask=AnodeGroup3ON; break;};
+   }
+
+   var32 |= AnodesGroupMask;
+
+   int curTube=AnodesGroup*2-2;
+   var32|=SymbolArray[stringToDisplay.substring(curTube, curTube+1).toInt()];
+   tmpVar=SymbolArray[stringToDisplay.substring(curTube+1, curTube+2).toInt()];
+
+   var32&=doEditBlink(curTube);  
+   tmpVar&=doEditBlink(curTube+1); 
+
+   var32 |= tmpVar<<10;
+
+   if (LD) var32&=~LowerDotsMask;
+    else  var32|=LowerDotsMask;
+  
+   if (UD) var32&=~UpperDotsMask; 
+    else  var32|=UpperDotsMask; 
+
+   digitalWrite(LEpin, LOW);    // allow data input (Transparent mode)
+
+   SPI.transfer(var32>>24); //[x][x][x][x][x][x][L1][L0]                - L0, L1 - dots
+   SPI.transfer(var32>>16); //[x][A2][A1][A0][RC9][RC8][RC7][RC6]       - A0-A2 - anodes
+   SPI.transfer(var32>>8);  //[RC5][RC4][RC3][RC2][RC1][RC0][LC9][LC8]  - RC9-RC0 - Right tubes cathodes
+   SPI.transfer(var32);     //[LC7][LC6][LC5][LC4][LC3][LC2][LC1][LC0]  - LC9-LC0 - Left tubes cathodes
+   //Serial.println(var32);
+
+   digitalWrite(LEpin, HIGH);     // latching data 
+
+   AnodesGroup=AnodesGroup+1;
+    if (AnodesGroup==4) {AnodesGroup=1;}
+  }
+}
+
+int doEditBlink(int pos)
+{
+  //if (pos==5) return 0; //need to be deleted for testing purpose only!
+  //return 0xFFFF;
+  if (!BlinkUp) return TubeOff;
+  if (!BlinkDown) return TubeOff;
+  int lowBit=blinkMask>>pos;
+  lowBit=lowBit&B00000001;
+  
+  static unsigned long lastTimeEditBlink=millis();
+  static bool blinkState=false;
+  word mask=TubeOn;
+  static int tmp=0;//blinkMask;
+  if ((millis()-lastTimeEditBlink)>300) 
+  {
+    lastTimeEditBlink=millis();
+    blinkState=!blinkState;
+
+    if (blinkState) tmp= 0;
+      else tmp=blinkMask;
+  }
+  if (((dotPattern&~tmp)>>6)&1==1) LD=true;//digitalWrite(pinLowerDots, HIGH);
+      else LD=false; //digitalWrite(pinLowerDots, LOW);
+  if (((dotPattern&~tmp)>>7)&1==1) UD=true; //digitalWrite(pinUpperDots, HIGH);
+      else UD=false; //digitalWrite(pinUpperDots, LOW);
+      
+  if ((blinkState==true) && (lowBit==1)) mask=TubeOff;//mask=B11111111;
+  return mask;
+}
+
+
+/*word doEditBlink(int pos)
+{
+  
+  if (!BlinkUp) return 0xFFFF;
+  if (!BlinkDown) return 0xFFFF;
+  //if (pos==5) return 0xFFFF; //need to be deleted for testing purpose only!
+  int lowBit=blinkMask>>pos;
+  lowBit=lowBit&B00000001;
+  
+  static unsigned long lastTimeEditBlink=millis();
+  static bool blinkState=false;
+  word mask=0xFFFF;
+  static int tmp=0;//blinkMask;
+  if ((millis()-lastTimeEditBlink)>300) 
+  {
+    lastTimeEditBlink=millis();
+    blinkState=!blinkState;
+    if (blinkState) tmp= 0;
+      else tmp=blinkMask;
+  }
+  if (((dotPattern&~tmp)>>6)&1==1) LD=true;//digitalWrite(pinLowerDots, HIGH);
+      else LD=false; //digitalWrite(pinLowerDots, LOW);
+  if (((dotPattern&~tmp)>>7)&1==1) UD=true; //digitalWrite(pinUpperDots, HIGH);
+      else UD=false; //digitalWrite(pinUpperDots, LOW);
+      
+  if ((blinkState==true) && (lowBit==1)) mask=0x3C00;//mask=B11111111;
+  //Serial.print("doeditblinkMask=");
+  //Serial.println(mask, BIN);
+  return mask;
+}*/
+
+
+
